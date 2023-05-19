a7d05833051bc4a6a1be7d83dee813cdece16200
Yixuan Su <syxistheone@gmail.com>
2023-05-19 11:10:11 +0100

    Delete code directory

--- code/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from .agent import DeepSpeedAgent
-from .openllama import OpenLLAMAPEFTModel
-
-def load_model(args):
-    agent_name = args['models'][args['model']]['agent_name']
-    model_name = args['models'][args['model']]['model_name']
-    model = globals()[model_name](**args)
-    agent = globals()[agent_name](model, args)
-    return agent
--- code/scripts/inference.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0 python inference_model.py \
-    --imagebind_ckpt_path ../pretrained_ckpt/imagebind_ckpt/ \
-    --vicuna_ckpt_path ../pretrained_ckpt/vicuna_ckpt/7b_v0/ \
-    --delta_ckpt_path ./ckpt/vicuna_7b_v0_peft/image_captioning_then_visual_instruction_stage_2//pytorch_model.pt \
-    --max_tgt_len 128 
